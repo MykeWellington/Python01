9d65bfe8e4514f4bc3e19dcdd3c45000b631cdf5
MykeWellington <110494807+MykeWellington@users.noreply.github.com>
2025-08-18 17:13:10 -0300

    Finalização do exercício.
    Terminamos o exercício de introdução ao Looping, inserindo for e while.

--- loopings.py
+++ loopings.py
@@ -1,1 +1,29 @@
-# Entendendo for e While, conceitos de loopings
+# Entendendo for e While, conceitos de loopings.
+palavra = 'Tangamandápio'
+contador = 0
+
+for letra in palavra:
+    print (str(contador) + ' - ' + letra)
+    contador = contador + 1
+    
+cidades = ['São Paulo', 'Osasco','Poá','São Bernardo do Campo']
+
+for cidade in cidades:
+    print(cidade)
+print('------------------------')    
+print(cidades[3])
+
+print('--------Trabalhando com While ------------')
+
+botao_executar = True # boolean true/false.
+contador = 0
+
+while botao_executar:
+    print(contador)
+    contador = contador + 1
+    if contador >= 100:
+        botao_executar = False
+        
+print('Cabô o looping!')
+
+    
